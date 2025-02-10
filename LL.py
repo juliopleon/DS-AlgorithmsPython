a78af107866d7ac95eede7cab3acb8eb0893a9a6
Julio Leon <juliopena143@icloud.com>
2025-02-10 15:10:54 -0700

    reverse ll

--- LL.py
+++ LL.py
@@ -111,10 +111,11 @@ def remove(self, index):
         return None
     if index == 0:
         return self.pop_first()
-    if index == self.length:
+    if index == self.length - 1:
         return self.pop()
     prev = self.get(index - 1)
     temp = prev.next
+    prev.next = temp.next
     temp.next = None
     self.length -= 1
     return temp
