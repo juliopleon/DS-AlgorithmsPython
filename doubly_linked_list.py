15bd831753e2ebbdc1ca5655ce5141f89a8242cf
Julio Leon <juliopena143@icloud.com>
2025-02-14 13:03:11 -0700

    DLL prepend method

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -37,8 +37,21 @@ class DoublyLinkedList:
         if self.length == 1:
             self.head = None
             self.tail = None
-        self.tail = self.tail.prev
-        self.tail.next = None
-        temp.prev = None
-        self.length -= 1
-        return temp
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+            self.length -= 1
+            return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
