31dd800e7cab569fc0489d2fbd2dbe77a8319aaf
Julio Leon <juliopena143@icloud.com>
2025-02-14 12:48:07 -0700

    pop method dll

--- LL.py
+++ LL.py
@@ -120,18 +120,6 @@ def remove(self, index):
     self.length -= 1
     return temp
 
-
-def reverse(self):
-    temp = self.head
-    self.head = self.tail
-    self.tail = temp
-    after = temp.next
-    before = None
-    for _ in range(self.length):
-        after = temp.next
-        temp.next = before
-        temp = after
-
 # common interview question reverse LL
 
 
