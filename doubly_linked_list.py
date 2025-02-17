0df07e16d0fc7a13f3413da11c6e1079a589c2b6
Julio Leon <juliopena143@icloud.com>
2025-02-17 15:31:29 -0700

    DLL remove method

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -121,18 +121,3 @@ class DoublyLinkedList:
         temp.prev = None
         self.length -= 1
         return temp
-
-    def remove(self, index):
-        if index < 0 or index >= self.length:
-            return False
-        if index == 0:
-            return self.pop_first()
-        if index == self.length - 1:
-            return self.pop()
-        temp = self.get(index - 1)
-        temp.next.prev = temp.prev
-        temp.prev.next = temp.next
-        temp.next = None
-        temp.prev = None
-        self.length -= 1
-        return temp
