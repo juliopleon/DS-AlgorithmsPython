195672c4dbc89cb7b6cf225676d7efba5506311f
Julio Leon <juliopena143@icloud.com>
2025-02-19 21:27:00 -0700

    method update

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -10,7 +10,7 @@ class DoublyLinkedList:
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
-        self.length += 1
+        self.length = 1
 
     def print_list(self):
         temp = self.head
